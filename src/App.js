d6625138b6db46f6b529b96a75f68d8ed3087884
joydeep-git <joydeep.accounts@tuta.io>
2022-06-07 16:07:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 function App() {
   return (
     <>
-    <h1>Welcome to React</h1>
+    <h1>Welcome </h1>
+    <greeting/>
     </>
   );
 }
-
+const message = function(){
+  return <>
+  <h4>How are you guys?</h4>
+  </>
+}
 export default App;
