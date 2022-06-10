40181620f8c4d6ba34572d04bcc4daa4002a04d2
joydeep-git <joydeep.accounts@tuta.io>
2022-06-10 11:30:24 +0530

    Css completed

--- src/component/Book.js
+++ src/component/Book.js
@@ -14,11 +14,10 @@ const Book = () => {
             </div>
             <h2 className="book-title">{title.toUpperCase()}</h2>
             <h4 className="book-price">{price}</h4>
-            <button className="button">add to cart</button>
+            <button className="button">Buy Now</button>
 
         </article>
     );
-    
 };
 
 export default Book;
